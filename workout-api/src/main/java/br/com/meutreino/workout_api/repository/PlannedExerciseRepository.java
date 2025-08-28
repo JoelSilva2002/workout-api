69dfaef5e5eb49e7055fa0c1e63755ae6166b2d9
Joel Silva <jsilvap2002@gmail.com>
2025-08-28 19:41:49 -0300

    Corrige estrutura de pastas do projeto

--- workout-api/src/main/java/br/com/meutreino/workout_api/repository/PlannedExerciseRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.meutreino.workout_api.repository;
-
-import br.com.meutreino.workout_api.model.PlannedExercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlannedExerciseRepository extends JpaRepository<PlannedExercise, Long> { }
-
